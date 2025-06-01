4312b1aa3f3d12611e8280fc5ce8f63cbe5a3070
Connor Zhan <yaalyy@outlook.com>
2025-06-01 02:16:09 +0100

    enhanced the exception handle to various OS & recognisation of chrome driver path

--- utils/driver_path.py
+++ utils/driver_path.py
@@ -1,6 +1,10 @@
 from .check_platform import detect_platform
 from pathlib import Path
 
+class UnsupportedOSException(Exception):
+    """Exception raised when the current OS is not supported."""
+    pass
+
 def get_driver_path():
     """
     Returns the chrome driver system path based on the current operating system.
@@ -11,24 +15,25 @@ def get_driver_path():
     
     system = detect_platform()
     if system == "unknown":
-        raise Exception("Unsupported platform detected. Please check your system configuration.")
+        raise UnsupportedOSException("Unsupported platform detected. Please check your system configuration.")
     elif system == "win32":  # Windows 32-bit
-        raise Exception("Win-32 detected, which is not supported for now.")
+        raise UnsupportedOSException("Win-32 detected, which is not supported for now.")
     elif system == "linux32": # Linux 32-bit
-        raise Exception("Linux-32 detected, which is not supported for now.")
+        raise UnsupportedOSException("Linux-32 detected, which is not supported for now.")
     elif system == "win64":  # Windows 64-bit
-        return str(Path("./chromedriver/win64/chromedriver.exe").resolve())
+        return str(Path(Path(__file__).parent.parent / "chromedriver/win64/chromedriver.exe").resolve())
     elif system == "linux64":  # Linux 64-bit
-        return str(Path("./chromedriver/linux64/chromedriver").resolve())
+        return str(Path(Path(__file__).parent.parent / "chromedriver/linux64/chromedriver").resolve())
     elif system == "macx64":
-        raise Exception("MacOSX64 detected, which is not supported for now.")
+        raise UnsupportedOSException("MacOSX64 detected, which is not supported for now.")
     elif system == "macarm64":
-        raise Exception("MacOSARM64 detected, which is not supported for now.")
+        raise UnsupportedOSException("MacOSARM64 detected, which is not supported for now.")
 
     
 if __name__ == "__main__":
     try:
         print(f"chrome driver path: {get_driver_path()}")
+    except UnsupportedOSException as e:
+        print(f"OS Error: {e}")
     except Exception as e:
-        print(f"Error: {e}")
-    
+        print(f"Unexpected error: {e}")
